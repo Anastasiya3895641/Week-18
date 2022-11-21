960fc04c0076edb3b8bddb587354870e53e6a593
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-11-21 22:34:37 +0300

    правки

--- 4_notion.js
+++ 4_notion.js
@@ -4,22 +4,21 @@
 
  let array = [];
 
-
  let createLi = (note) => {
+
     let li = document.createElement('li');
     li.textContent = note;
     list.append(li);
  };
 
  form.addEventListener('submit',function(event){
+
     event.preventDefault();
     array.push(input.value);
     localStorage.setItem(Math.random(), array);
-    createLi(input.value)
+    createLi(input.value);
     input.value = "";
  });
-
-
 
  array.forEach(element =>
     {
