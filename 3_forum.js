e149e08c2ebef253a9725d059e9d21c4bcd464d6
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-11-22 19:19:51 +0300

    4 задание готово

--- 3_forum.js
+++ 3_forum.js
@@ -1,29 +1,40 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    event.preventDefault();
     let name = localStorage.getItem('name');
+    let foto = localStorage.getItem('foto');
+
     if (name != null){
         document.getElementById('author').value = name;
+    };
+
+    
+    if (foto != null){
+        document.getElementById('ava').value = foto;
     };
     
 });
 
-function sendMessage(author, comment){
-    document.getElementById("message").innerHTML = `<span>${author}:</span> ${comment}`;
+function sendMessage(ava, author, comment){
+    document.getElementById("message").innerHTML += `<img width=80vw height=80vh src="${ava}" alt="user Icon"><span>${author}:</span> ${comment}`;
 };
 
 function checkMessage() {
     
     let author = document.getElementById("author").value;
     let comment = document.getElementById('comment').value;
+    let ava = document.getElementById('ava').value;
 
     
 
     if (localStorage.getItem('name') == null) {
         localStorage.setItem('name', author);
-    }
+    };
 
-    sendMessage(author, comment);
+    if (localStorage.getItem('foto') == null) {
+        localStorage.setItem('foto', ava);
+    };
+
+    sendMessage(ava, author, comment);
 };
 
